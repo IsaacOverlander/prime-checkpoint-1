eeb617c48dbdf0827f5e014bc3e031ef42957b64
Unknown <Isaac.overlander@gmail.com>
2018-07-30 13:23:45 -0500

    create a <p> element to count number of times the button was pressed

--- client.js
+++ client.js
@@ -1,10 +1,14 @@
 $(document).ready(readyNow);
 
+let numberClicked = 0
+
 function readyNow() {
     $('#generate').on('click', addDiv);
     
 }
 
 function addDiv() {
-    $('#container').append('<div id="content"></div>');
+    numberClicked += 1;
+    $('#container').html('<div id="content"></div>');
+    $('#content').html('<p>' + numberClicked + '</p>');
 }
